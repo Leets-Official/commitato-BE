8fe9e4cc4a81bfbe7e362bdd6287d00c032c707d
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-07-13 03:55:20 +0900

    feat: 유저 컨트롤러 생성

---
+++ src/main/java/com/leets/commitatobe/domain/user/presentation/UserController.java
@@ -0,0 +1,12 @@
+package com.leets.commitatobe.domain.user.presentation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+}
