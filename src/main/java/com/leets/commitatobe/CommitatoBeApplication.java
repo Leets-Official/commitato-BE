4bacd89364d2cb1faf20c51f7677cd301c8f8583
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-07-13 02:09:47 +0900

    Initial commit

---
+++ src/main/java/com/leets/commitatobe/CommitatoBeApplication.java
@@ -0,0 +1,13 @@
+package com.leets.commitatobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitatoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitatoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leets/commitatobe/CommitatoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leets.commitatobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitatoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
