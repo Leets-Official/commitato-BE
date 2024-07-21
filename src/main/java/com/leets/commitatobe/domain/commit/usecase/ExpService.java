5ee19fe43a6636f36525cfa5b8413f4ea2b8f432
1winhyun <jimking1@naver.com>
2024-07-21 18:21:06 +0900

    chore: 가독성 및 성능을 위한 코드 수정

--- src/main/java/com/leets/commitatobe/domain/commit/usecase/ExpService.java
+++ src/main/java/com/leets/commitatobe/domain/commit/usecase/ExpService.java
@@ -26,8 +26,7 @@ public class ExpService {
     public void calculateAndSaveExp(String githubId){
         User user=userRepository.findByGithubId(githubId)
                 .orElseThrow(()->new UsernameNotFoundException("해당하는 깃허브 닉네임과 일치하는 유저를 찾을 수 없음: " +githubId));
-        List<Commit> commits=commitRepository.findAllByUser(user);//사용자의 모든 커밋을 불러온다.
-        commits.sort(Comparator.comparing(Commit::getCommitDate));//오름차순으로 정렬
+        List<Commit> commits=commitRepository.findAllByUserOrderByCommitDateAsc(user);//사용자의 모든 커밋을 날짜 오름차순으로 불러온다.
 
         int consecutiveDays=0;//연속 커밋 일수
         LocalDateTime lastCommitDate=null;//마지막 커밋 날짜
@@ -35,7 +34,7 @@ public class ExpService {
 
         for(Commit commit:commits){//각 커밋을 반복해서 계산
             if(commit.isCalculated()) continue;//이미 계산된 커밋
-            LocalDateTime commitDate=commit.getCommitDate().toLocalDate().atStartOfDay();//커밋날짜를 가져와 시간 설정
+            LocalDateTime commitDate=commit.getCommitDate();//커밋날짜를 가져와 시간 설정
 
             if(lastCommitDate != null && commitDate.isEqual(lastCommitDate.plusDays(1))){
                 // 마지막 커밋 날짜가 존재하고, 현재 커밋 날짜가 마지막 커밋 날짜의 다음 날과 같으면 연속 커밋으로 간주합니다.
@@ -48,7 +47,7 @@ public class ExpService {
             int bonusExp=consecutiveDays*5;//보너스 경험치 계산
             totalExp+=1+bonusExp;//총 경험치 업데이트
 
-            commit.setCalculated(true);//커밋 계산 여부를 true로 해서 다음 게산에서 제외
+            commit.isCommitCalculated(true);//커밋 계산 여부를 true로 해서 다음 게산에서 제외
             lastCommitDate=commitDate;//마지막 커밋날짜를 현재 커밋날짜로 업데이트
         }
         user.setExp(totalExp);//사용자 경험치 업데이트
