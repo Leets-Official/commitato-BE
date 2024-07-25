f38d26728ef9e72a3f020ff4b665fa9cfd07cbd3
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-07-26 03:28:32 +0900

    refactor: 티어 예외 처리 로직 변경

--- src/main/java/com/leets/commitatobe/domain/user/usecase/UserQueryServiceImpl.java
+++ src/main/java/com/leets/commitatobe/domain/user/usecase/UserQueryServiceImpl.java
@@ -9,14 +9,11 @@ import com.leets.commitatobe.domain.user.presentation.dto.response.UserRankRespo
 import com.leets.commitatobe.domain.user.presentation.dto.response.UserSearchResponse;
 import com.leets.commitatobe.global.exception.ApiException;
 import com.leets.commitatobe.global.response.code.status.ErrorStatus;
-import com.leets.commitatobe.global.utils.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 import static com.leets.commitatobe.global.response.code.status.ErrorStatus._USER_NOT_FOUND;
 
@@ -27,21 +24,25 @@ import static com.leets.commitatobe.global.response.code.status.ErrorStatus._USE
 public class UserQueryServiceImpl implements UserQueryService {
     private final UserRepository userRepository;
     private final ExpService expService;
-    private final JwtProvider jwtProvider;
     private final LoginCommandService loginCommandService;
 
     @Override
     @Transactional
     public UserSearchResponse searchUsersByGithubId(String githubId) {// 유저 이름으로 유저 정보 검색
-        User user=userRepository.findByGithubId(githubId)
-                .orElseThrow(()->new ApiException(ErrorStatus._USER_NOT_FOUND));
+        User user = userRepository.findByGithubId(githubId)
+                .orElseThrow(() -> new ApiException(ErrorStatus._USER_NOT_FOUND));
         expService.calculateAndSaveExp(user.getGithubId());
-        Tier tier=user.getTier();
+        Tier tier = user.getTier();
+
+        if(tier == null) {
+            throw new ApiException(ErrorStatus._TIER_NOT_FOUND);
+        }
+
         return new UserSearchResponse(
                 user.getUsername(),
                 user.getExp(),
-                tier!=null?tier.getTierName():"Unranked",
-                tier!=null?tier.getCharacterUrl():null,
+                tier.getTierName(),
+                tier.getCharacterUrl(),
                 user.getConsecutiveCommitDays(),
                 user.getTotalCommitCount(),
                 user.getTodayCommitCount()
@@ -49,29 +50,30 @@ public class UserQueryServiceImpl implements UserQueryService {
     }
 
     @Override
-    public Page<UserRankResponse> getUsersByExp(Pageable pageable){//경험치 순으로 페이징된 유저 정보 조회
-        Page<User> userPage= userRepository.findAllByOrderByExpDesc(pageable);
+    public Page<UserRankResponse> getUsersByExp(Pageable pageable) {//경험치 순으로 페이징된 유저 정보 조회
+        Page<User> userPage = userRepository.findAllByOrderByExpDesc(pageable);
 
-        return userPage.map(user->{// 각 사용자의 경험치 최신화 및 UserRankResponse 변환
+        return userPage.map(user -> {// 각 사용자의 경험치 최신화 및 UserRankResponse 변환
             expService.calculateAndSaveExp(user.getGithubId());// 경험치 계산 및 저장
-            Tier tier=user.getTier();
+
+            Tier tier = user.getTier();
+
             return new UserRankResponse(
                     user.getUsername(),
                     user.getExp(),
                     user.getConsecutiveCommitDays(),
-                    tier!=null?tier.getTierName():"Unranked");
+                    tier != null ? tier.getTierName() : "not Defined");
         });
     }
 
     @Override
     public String getUserGitHubAccessToken(String githubId) {
-        User user=userRepository.findByGithubId(githubId).orElseThrow(()->new ApiException(_USER_NOT_FOUND));
+        User user = userRepository.findByGithubId(githubId).orElseThrow(() -> new ApiException(_USER_NOT_FOUND));
 
         String gitHubAccessToken = user.getGitHubAccessToken();
 
         return loginCommandService.decrypt(gitHubAccessToken);
     }
-
 
 
 }
--- src/main/java/com/leets/commitatobe/global/response/code/status/ErrorStatus.java
+++ src/main/java/com/leets/commitatobe/global/response/code/status/ErrorStatus.java
@@ -40,8 +40,11 @@ public enum ErrorStatus implements BaseErrorCode {
     // 디코딩 오류
     _DECRYPT_ERROR(HttpStatus.BAD_REQUEST, "DECRYPT_001", "토큰 디코딩 과정에서 오류가 발생했습니다."),
 
-    //검색 기능 관련
-    _USER_NOT_FOUND(HttpStatus.NOT_FOUND,"USER_001","해당 유저가 존재하지 않습니다");
+    // 검색 기능 관련
+    _USER_NOT_FOUND(HttpStatus.NOT_FOUND,"USER_001","해당 유저가 존재하지 않습니다"),
+
+    // 티어 관련
+    _TIER_NOT_FOUND(HttpStatus.NOT_FOUND, "TIER_001", "해당하는 티어를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
