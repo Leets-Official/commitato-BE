f80a7cd6f75d93391dafe43168091931b342bf0b
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-07-26 15:48:36 +0900

    refactor: 랭킹 페이지에 커스텀 페이지 적용

--- src/main/java/com/leets/commitatobe/domain/user/usecase/UserQueryService.java
+++ src/main/java/com/leets/commitatobe/domain/user/usecase/UserQueryService.java
@@ -3,11 +3,10 @@ package com.leets.commitatobe.domain.user.usecase;
 
 import com.leets.commitatobe.domain.user.presentation.dto.response.UserRankResponse;
 import com.leets.commitatobe.domain.user.presentation.dto.response.UserSearchResponse;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.leets.commitatobe.global.shared.CustomPage;
 
 public interface UserQueryService {
     UserSearchResponse searchUsersByGithubId(String GithubId);//유저 이름으로 유저 정보를 검색하는 메서드
-    Page<UserRankResponse> getUsersByExp(Pageable pageable);//경험치 순으로 페이징된 유저 정보를 조회
+    CustomPage<UserRankResponse> getUsersOrderByExp(int page);//경험치 순으로 페이징된 유저 정보를 조회
     String getUserGitHubAccessToken(String githubId);
 }
