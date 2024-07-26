1455ee4876b7adbb40d39ef3732435a7b30d0cc7
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-07-26 15:48:36 +0900

    refactor: 랭킹 페이지에 커스텀 페이지 적용

--- src/main/java/com/leets/commitatobe/domain/user/usecase/UserQueryService.java
+++ src/main/java/com/leets/commitatobe/domain/user/usecase/UserQueryService.java
@@ -7,6 +7,8 @@ import com.leets.commitatobe.global.response.CustomPageResponse;
 
 public interface UserQueryService {
     UserSearchResponse searchUsersByGithubId(String GithubId);//유저 이름으로 유저 정보를 검색하는 메서드
+
     CustomPageResponse<UserRankResponse> getUsersOrderByExp(int page, int size);//경험치 순으로 페이징된 유저 정보를 조회
+
     String getUserGitHubAccessToken(String githubId);
 }
