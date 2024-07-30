d0499d1735874bbef78bf6b5cfbf7c0db1a1c4a2
zzu-yaaa <jy122032@gmail.com>
2024-07-30 13:01:53 +0900

    response dto 수정

--- src/main/java/com/leets/commitatobe/domain/user/presentation/dto/response/UserSearchResponse.java
+++ src/main/java/com/leets/commitatobe/domain/user/presentation/dto/response/UserSearchResponse.java
@@ -1,13 +1,10 @@
 package com.leets.commitatobe.domain.user.presentation.dto.response;
 
 public record UserSearchResponse(
-        String username,
-        Integer exp,
+        Integer ranking,
+        String githubId,
         String tierName,
-        String characterUrl,
-        Integer consecutiveCommitDays,
-        Integer totalCommitCount,
-        Integer todayCommitCount,
-        Integer ranking
+        Integer exp,
+        Integer consecutiveCommitDays
 ) {
 }
