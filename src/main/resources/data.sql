e4685e45c19713f865da39355431025bea50b1dd
1winhyun <jimking1@naver.com>
2025-02-25 16:09:04 +0900

    refactor: 티어 sql 기준 경험치 값 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 INSERT INTO tier (tier_id, tier_name, character_url, required_exp)
 VALUES (UNHEX(REPLACE(UUID(), '-', '')), '바보 감자', 'https://d1ds22ndweg1nu.cloudfront.net/potato/stupidPotato.svg', 0),
-       (UNHEX(REPLACE(UUID(), '-', '')), '말하는 감자', 'https://d1ds22ndweg1nu.cloudfront.net/potato/speakingPotato.svg', 1000),
-       (UNHEX(REPLACE(UUID(), '-', '')), '개발자 감자', 'https://d1ds22ndweg1nu.cloudfront.net/potato/developerPotato.svg', 15000),
-       (UNHEX(REPLACE(UUID(), '-', '')), 'CEO 감자', 'https://d1ds22ndweg1nu.cloudfront.net/potato/CEOPotato.svg', 33000);
+       (UNHEX(REPLACE(UUID(), '-', '')), '말하는 감자', 'https://d1ds22ndweg1nu.cloudfront.net/potato/speakingPotato.svg',
+        30000),
+       (UNHEX(REPLACE(UUID(), '-', '')), '개발자 감자', 'https://d1ds22ndweg1nu.cloudfront.net/potato/developerPotato.svg',
+        150000),
+       (UNHEX(REPLACE(UUID(), '-', '')), 'CEO 감자', 'https://d1ds22ndweg1nu.cloudfront.net/potato/CEOPotato.svg',
+        330000);
