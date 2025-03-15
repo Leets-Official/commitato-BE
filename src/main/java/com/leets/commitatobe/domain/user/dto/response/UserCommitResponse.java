7bba2bcf12cfecfabdd0b09f2ff4eb4029e72597
Hanseunghyun <hanseunghyun@HanseuncBookPro.Davolink>
2025-03-15 16:33:31 +0900

    refactor: UserCommitResponse 필요없는 어노테이션 제거

--- src/main/java/com/leets/commitatobe/domain/user/dto/response/UserCommitResponse.java
+++ src/main/java/com/leets/commitatobe/domain/user/dto/response/UserCommitResponse.java
@@ -2,9 +2,6 @@ package com.leets.commitatobe.domain.user.dto.response;
 
 import java.time.LocalDateTime;
 
-import lombok.Builder;
-
-@Builder
 public record UserCommitResponse(
 	LocalDateTime commitDate,
 	Integer cnt
