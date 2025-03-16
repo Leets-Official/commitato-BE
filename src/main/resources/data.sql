962551e0b11e3e7c2a76693a12257735abc67313
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-03-16 22:41:57 +0900

    refactor: 기획 변경으로 인한 경험치 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,4 @@ VALUES (UNHEX(REPLACE(UUID(), '-', '')), '바보 감자', 'https://commitato.s3.
        (UNHEX(REPLACE(UUID(), '-', '')), '개발자 감자', 'https://commitato.s3.ap-northeast-2.amazonaws.com/developerPotato.svg',
         150000),
        (UNHEX(REPLACE(UUID(), '-', '')), 'CEO 감자', 'https://commitato.s3.ap-northeast-2.amazonaws.com/CEOPotato.svg',
-        330000);
+        300000);
